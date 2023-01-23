d370037039436d0c05205e3ba798b0429d7fe66f
takosuke <cyberaura_0211@yahoo.co.jp>
2023-01-23 22:50:14 +0900

    add scraping script to get images without extentions

--- scraping.py
+++ scraping.py
@@ -2,9 +2,11 @@
 import urllib.request #拡張子のないURLから画像を保存
 import requests # urlを読み込むためrequestsをインポート
 from bs4 import BeautifulSoup # htmlを読み込むためBeautifulSoupをインポート
+import csv
 
 URL = 'https://cookpad.com/recipe/4993029/' # URL入力
-images = [] # 画像リストの配列
+images = [] # 画像リストの配列]
+classes = []
 
 soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
 
@@ -12,18 +14,19 @@ print(soup.find_all("img")) # 確認
 
 for link in soup.find_all("img"): # imgタグを取得しlinkに格納
     if link.get("src"): # imgタグ内の.jpgであるsrcタグを取得
-        images.append(link.get("src").split('.png')[0]) # imagesリストに格納
+        images.append(link.get("src"))
+        classes.append(link.get("class"))
 
+with open('test.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    for i, img in enumerate(images):
+        writer.writerow([i, classes[i], img])
 
+for i, url in enumerate(images):
+    dl = urllib.request.urlopen(url).read()
 
-# urlopen
+    # ファイルへの保存
+    save_name = 'img/' + str(i) + '.png'
+    with open(save_name, "wb") as f:
+        f.write(dl)
 
-
-url = 'https://img.cpcdn.com/steps/24575634/m/585a7c13f704aea125df3b176a4638c2?u=3501781&amp;p=1523845646'
-# urlの処理
-dl = urllib.request.urlopen(url).read()
-
-# ファイルへの保存
-save_name = "a.png"
-with open(save_name, "wb") as f:
-    f.write(dl)
