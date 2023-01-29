35f711f60c441dccbb910708360623a03b206153
takosuke <cyberaura_0211@yahoo.co.jp>
2023-01-29 23:26:36 +0900

    some change

--- scraping.py
+++ scraping.py
@@ -8,25 +8,34 @@ URL = 'https://cookpad.com/recipe/4993029/' # URL入力
 images = [] # 画像リストの配列]
 classes = []
 
-soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
+def getimages():
 
-print(soup.find_all("img")) # 確認
+    soup = BeautifulSoup(requests.get(URL).content,'lxml') # bsでURL内を解析
 
-for link in soup.find_all("img"): # imgタグを取得しlinkに格納
-    if link.get("src"): # imgタグ内の.jpgであるsrcタグを取得
-        images.append(link.get("src"))
-        classes.append(link.get("class"))
+    print(soup.find_all("img")) # 確認
 
-with open('test.csv', 'w', newline="") as f:
-    writer = csv.writer(f)
-    for i, img in enumerate(images):
-        writer.writerow([i, classes[i], img])
+    for link in soup.find_all("img"): # imgタグを取得しlinkに格納
+        if link.get("src"): # imgタグ内の.jpgであるsrcタグを取得
+            images.append(link.get("src"))
+            classes.append(link.get("class"))
 
-for i, url in enumerate(images):
-    dl = urllib.request.urlopen(url).read()
+    with open('test.csv', 'w', newline="") as f:
+        writer = csv.writer(f)
+        for i, img in enumerate(images):
+            writer.writerow([i, classes[i], img])
 
-    # ファイルへの保存
-    save_name = 'img/' + str(i) + '.png'
-    with open(save_name, "wb") as f:
-        f.write(dl)
+    for i, url in enumerate(images):
+        dl = urllib.request.urlopen(url).read()
 
+        # ファイルへの保存
+        save_name = 'img/' + str(i) + '.png'
+        with open(save_name, "wb") as f:
+            f.write(dl)
+
+
+
+
+soup = BeautifulSoup(requests.get(URL).content,'lxml')
+found = soup.findAll('div', class_='ingredient_name')
+
+print(len(found))
