13bbf7db8ea1dd4dab7079d077d32e54897c00d7
takosuke <cyberaura_0211@yahoo.co.jp>
2023-02-13 21:16:35 +0900

    change scraping.py for delishkitchen data

--- app/api.py
+++ app/api.py
@@ -7,32 +7,28 @@ from . import browser_call
 
 api = Flask(__name__, static_folder='./static')
 
-cookpad_data = scraping.cookpadData()
+recipe_data = scraping.recipeData()
 
 # スマホに表示する画面
 @api.route('/', methods=['GET'])
 def index():
-    cookpad_data.clearData()
+    recipe_data.clearData()
     return render_template('index.html')
 
 # URLを受け取って、スクレイピング
 @api.route('/post_url', methods=['POST'])
 def recieve_url():
 
-    cookpad_data.clearData()
+    recipe_data.clearData()
 
     # set last-index
-    cookpad_data.step_last_index = int(request.form["index"])
-    print(cookpad_data.step_last_index)
+    recipe_data.step_last_index = int(request.form["index"])
+    print(recipe_data.step_last_index)
 
     url = request.form["field"]
     print(url)
 
-    if url[-1] == '/':
-        url = url[:-1]
-
-    # URLが誤り
-    if url.split('/')[-2] != 'recipe':
+    if recipe_data.checkURL(url) == False:
         print("INVALID URL")
         return render_template('index.html')
 
@@ -43,11 +39,11 @@ def recieve_url():
     #    os.makedirs('app' + IMG_DIR)
 
     # スクレイピング処理
-    cookpad_data.scraping(url)
-    print(cookpad_data.title)
+    recipe_data.scraping()
+    print(recipe_data.title)
 
     #open html in browser
-    render_template('projection.html', data = cookpad_data)
+    render_template('projection.html', data = recipe_data)
     browser_call.call_selenium_browser('http://0.0.0.0:8000/projection')
 
     # スクレイピングが完了したら、PC側でブラウザを起動
@@ -56,7 +52,7 @@ def recieve_url():
 # プロジェクタ用の表示画面のURL
 @api.route('/projection', methods=['GET'])
 def projection_data():
-    return render_template('projection.html', data = cookpad_data)
+    return render_template('projection.html', data = recipe_data)
 
 # プロジェクタ用の表示画面のURL
 @api.route('/black', methods=['GET'])
--- app/scraping.py
+++ app/scraping.py
@@ -5,11 +5,13 @@ import csv
 import time
 
 
-class cookpadData():
+class recipeData():
 
     def __init__(self):
 
         self.url = ""
+
+        self.service_name = ""
 
         self.title = ""
         self.author = ""
@@ -24,14 +26,13 @@ class cookpadData():
         self.step_last_index = 100
         self.step_texts = []
 
-
         self.step_img_paths = []
         self.main_img_path = ""
-
+        self.main_video_path = ""
 
     def clearData(self):
         self.url = ""
-        self.platform_name = ""
+        self.service_name = ""
         self.title = ""
         self.author = ""
         self.ingredient_length = 0
@@ -42,81 +43,103 @@ class cookpadData():
         self.step_texts = []
         self.step_img_paths = []
         self.main_img_path = ""
+        self.main_video_path = ""
 
-
-    def checkURL():
-        return False
-
-    def scraping(self, url):
+    def checkURL(self, url):
         self.url = url
+
+        if url.split('/')[2] == 'delishkitchen.tv':
+            self.platform_name = 'dk'
+            return True
+        elif url.split('/')[2] == 'cookpad.com':
+            self.platform_name = 'cp'
+            return True
+        else:
+            return False
+
+    def scraping(self):
 
         soup = BeautifulSoup(requests.get(self.url).content,'lxml')
         print("Access...", self.url)
+           
         time.sleep(1)
 
-        self.getMetaData(soup)
-        self.getIngredients(soup)
-        self.getSteps(soup)
+        if self.platform_name == 'cp':
+            self.cp_getMetaData(soup)
+            self.cp_getIngredients(soup)
+            self.cp_getSteps(soup)
 
+        elif self.platform_name == 'dk':
+            self.dk_getMetaData(soup)
+            self.dk_getIngredients(soup)
+            self.dk_getSteps(soup)
 
-    def getIngredients(self, soup):
-        ingredient_names = soup.findAll('div', class_='ingredient_name')
-        ingredient_amounts = soup.findAll('div', class_='ingredient_quantity amount')
+    #cookpad
+    def cp_getIngredients(self, soup):
+        ingredient_names = soup.find_all('div', class_='ingredient_name')
+        ingredient_amounts = soup.find_all('div', class_='ingredient_quantity amount')
         self.ingredient_length = len(ingredient_amounts)
         for i in range(len(ingredient_names)):
             self.ingredient_amounts.append(ingredient_amounts[i].get_text())
             self.ingredient_names.append(ingredient_names[i].get_text())
 
-    def getSteps(self, soup):
+    #cookpad
+    def cp_getSteps(self, soup):
 
         # get step text
-        step_texts = soup.findAll(class_='step_text')
-
+        step_texts = soup.find_all(class_='step_text')
         self.step_length = min(len(step_texts), self.step_last_index)
-
         for i in range(self.step_length):
             self.step_texts.append(step_texts[i].get_text().replace('\n', ''))
 
         # get step image
-        steps = soup.findAll(class_=['step','step_last'])
+        steps = soup.find_all(class_=['step','step_last'])
         for i in range(self.step_length):
             if steps[i].find('img') is None:
                 self.step_img_paths.append('none')
             else:
                 self.step_img_paths.append(steps[i].find('img')['src'])
 
-    def getMetaData(self, soup):
+    #cookpad
+    def cp_getMetaData(self, soup):
         self.title = soup.find(class_='recipe-title').get_text().replace('\n', '')
-        author = soup.find(class_='author recipe_author_container')#.get_text().replace('\n', '')
-        if author is None:
-            author = soup.find(class_='author')
-        if author is not None:
-            self.author = author.get_text().replace('\n', '')
+        self.author = soup.find(class_='author recipe_author_container').get_text().replace('\n', '')
         self.main_img_path = soup.find(class_='photo large_photo_clickable')['src']
-
     
-    # not using now
-    """
-    def getimages(self):
+    # Delish kitchen
+    def dk_getIngredients(self, soup):
+        ingredient_names = soup.find_all(class_='ingredient-name')
+        ingredient_amounts = soup.find_all(class_='ingredient-serving')
+        self.ingredient_length = len(ingredient_amounts)
+        for i in range(len(ingredient_names)):
+            self.ingredient_amounts.append(ingredient_amounts[i].get_text())
+            self.ingredient_names.append(ingredient_names[i].get_text())
 
-        dl = urllib.request.urlopen(self.main_img_path).read()
-        # ファイルへの保存
-        save_name = 'app' + self.imgDir + '/main.png'
-        self.main_img_path = '.' + self.imgDir + '/main.png'
-        with open(save_name, 'wb') as f:
-            f.write(dl)
+    # Delish kitchen
+    def dk_getSteps(self, soup):
 
-        for i, url in enumerate(self.step_img_paths):
-            if url == 'none':
-                self.step_img_paths.append("none")
-                continue
-            dl = urllib.request.urlopen(url).read()
-            time.sleep(1)
-            # ファイルへの保存
-            self.step_img_paths.append('.' + self.imgDir + '/step_' + str(i) + '.png')
-            save_name = 'app' + self.imgDir + '/step_' + str(i) + '.png'
-            with open(save_name, 'wb') as f:
-                f.write(dl)
-    """
+        # get step text
+        step_texts = soup.find_all(class_='step-desc')
+        self.step_length = min(len(step_texts), self.step_last_index)
 
+        for i in range(self.step_length):
+            self.step_texts.append(step_texts[i].get_text().replace('\n', ''))
+
+        # get step image
+        steps = soup.find_all(class_=['step','step_last'])
+        for i in range(self.step_length):
+            if steps[i].find('img') is None:
+                self.step_img_paths.append('none')
+            else:
+                self.step_img_paths.append(steps[i].find('img')['src'])
+
+    # Delish kitchen
+    def dk_getMetaData(self, soup):
+        self.title = soup.select_one('h1.title').get_text().replace('\n', '') #セレクタを使用　<tag>.<class> (スペース不要)
+        self.author = soup.select_one('p.name').get_text().replace('\n', '')
+        videos = soup.find_all('source')
+        for v in videos:
+            if v.get('src') is not None: #srcを持たない要素はNoneとして抽出されている。
+                if v.get('src')[-3:] == 'mp4':
+                    self.main_video_path = v.get('src')
 
