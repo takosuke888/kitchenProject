4b55930771e59d3dc611a064520c0fe7bd646d42
takosuke <cyberaura_0211@yahoo.co.jp>
2023-08-23 17:30:34 +0900

    make imageviewer service

--- app/getImagePath.py
+++ app/getImagePath.py
@@ -5,11 +5,10 @@ def getImgPath():
 
     types = ('jpg', 'png', 'JPG')
     files = []
-    paths = []
     for t in types:
         files += glob.glob('/home/takosuke/kitchenProject/app/static/img/*.' + t)
 
     random.shuffle(files)
 
-    return paths
+    return files
     
