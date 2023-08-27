9e88e55bf43d64cd73464f67d4c3fcaa9032518d
takosuke <cyberaura_0211@yahoo.co.jp>
2023-08-27 13:49:42 +0900

    change image-viewer style

--- ImageShrinker.py
+++ ImageShrinker.py
@@ -7,12 +7,17 @@ for t in types:
     files += glob.glob('./app/static/src/*.' + t)
 
 for file in files:
-    img = cv2.imread(file, 1)
-    h = img.shape[0]
-    w = img.shape[1]
-    w2 = 1280
-    h2 = round(h * 1280 / w )
-    resized = cv2.resize(img, (w2, h2))
-    filename = file.split('\\')[-1]
-    newname = './app/static/img/' + filename
-    cv2.imwrite(newname, resized)
+    try:
+        img = cv2.imread(file, 1)
+        h = img.shape[0]
+        w = img.shape[1]
+        w2 = 1280
+        h2 = round(h * 1280 / w )
+        resized = cv2.resize(img, (w2, h2))
+        filename = file.split('\\')[-1]
+        newname = './app/static/img/' + filename
+        print(newname)
+        cv2.imwrite(newname, resized)
+    except:
+        print('Error: ', file)
+        pass
